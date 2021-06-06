5d680b0973bed345d7bed612052ce71c0a4f972c
GusevTimofey <gusev.t.s1@gmail.com>
2021-06-06 14:20:20 +0300

    added plutus to the project

--- src/Dex/BlocksProcessor.hs
+++ src/Dex/BlocksProcessor.hs
@@ -7,12 +7,13 @@ import Dex.Models.Rosetta.Response.BlockResponse (BlockResponse)
 import RIO
 import RIO.Map as Map
 import RIO.Map (Map)
+import Plutus.Contract.Schema ()
 
 -- add fork checker (sync with current processor)
 -- add cache (redis) in case to store last max height and current unused outputs
 -- check if txn's outputs contain datum hash with proxy contract
 run :: Int -> IORef (Map Text BlockResponse) -> IO ()
-run ref = do
+run height ref = do
     r <- getBlock height
     _ <- print r
     run (height + 1) ref
