4866c52b0c17dae9b87cbb19bdfff90abc746211
GusevTimofey <gusev.t.s1@gmail.com>
2021-06-17 15:11:40 +0300

    monorepo init

---
+++ tracker/app/Main.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Dex.TxOutsProcessor ( run )
+import Dex.Models.AppSettings 
+    ( HttpSettings(..)
+    , BlockRequestSettings(..)
+    , KafkaProducerSettings(..)
+    , AppSettings(..)
+    )
+import RIO ( runRIO )
+
+main :: IO ()
+main = do
+    appSettings <- readSettings
+    runRIO appSettings $ do run
+
+readSettings :: IO AppSettings
+readSettings = do
+    let httpSs = HttpSettings "0.0.0.0" 8081 
+        blockRequestS = BlockRequestSettings 0
+        kafkaSs = KafkaProducerSettings "amm-topic" "proxy-topic" ["0.0.0.0:9092"] "default-proxy-key" "default-amm-key" 
+    pure $ AppSettings httpSs blockRequestS kafkaSs
