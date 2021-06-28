c2d1bdd3d6d92f0142e0a7036df7ed166ebb3f9c
GusevTimofey <gusev.t.s1@gmail.com>
2021-06-28 21:45:53 +0300

    http server added

--- pools-resolver/src/Resolver/HttpServer.hs
+++ pools-resolver/src/Resolver/HttpServer.hs
@@ -1,21 +1,31 @@
-module Resolver.HttpServer where
+module Resolver.HttpServer
+    ( app
+    ) where
 
-import RIO
+import           Control.Monad.IO.Class       (liftIO)
+import RIO (Maybe, ($))
+import Data.Int
 import Dex.Models
-import Resolver.PoolsResolver
+import Resolver.PoolsResolver (resolve)
+import           Servant
+import           Servant.API
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import RIO.ByteString
 
 type Api =
-    "resolve" :> Capture "poolId" Int :> Get '[JSON] [Pool]
+    "resolve" :> Capture "poolId" PoolId :> Get '[JSON] (Maybe Pool)
 
 apiProxy :: Proxy Api
 apiProxy = Proxy
 
 app :: Application
-app = serve apiProxyy server
+app = serve apiProxy server
 
 server :: Server Api
 server =
+    resolvePool
 
 resolvePool :: PoolId -> Handler (Maybe Pool)
-resolvePool = 
-    liftIO $ 
+resolvePool pId = 
+    liftIO $ resolve pId
