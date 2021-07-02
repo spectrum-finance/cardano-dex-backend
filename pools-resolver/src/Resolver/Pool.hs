018320185b1aeec9bd1dbdd667b94b31985ad2bd
GusevTimofey <gusev.t.s1@gmail.com>
2021-07-02 22:10:39 +0300

    redis draft

--- pools-resolver/src/Resolver/Pool.hs
+++ pools-resolver/src/Resolver/Pool.hs
@@ -11,6 +11,7 @@ import Dex.Models
 import Resolver.Models.CfmmPool
 import Plutus.V1.Ledger.TxId
 import qualified PlutusTx.Builtins as Builtins
+import RIO.ByteString.Lazy as BS
 
 mkLastPredictedKey :: PoolId -> String
 mkLastPredictedKey (PoolId id) = "last_predicted_" ++ "box_id"--Builtins.decodeUtf8 id
@@ -22,7 +23,11 @@ mkPredictedNext :: PoolId -> TxId -> Integer -> String
 mkPredictedNext (PoolId id) (TxId txId) gix = "predicted_next_" ++ (show txId) ++ (show gix) ++ "_" ++ "box_id" --Builtins.decodeUtf8 id
 
 putPredicted :: PredictedPool Pool -> IO ()
-putPredicted _ = undefined 
+putPredicted (PredictedPool pool) = do
+     conn <- checkedConnect defaultConnectInfo
+     runRedis conn $ do
+         set (mkLastConfirmedKey $ poolId pool) (BS.toStrict . encode pool)
+         set (mkPredictedNext $ poolId pool) (BS.toStrict . encode pool)
 
 putConfirmed :: ConfirmedPool Pool -> IO ()
 putConfirmed _ = undefined
