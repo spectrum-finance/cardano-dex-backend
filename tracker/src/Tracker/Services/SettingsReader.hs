09b7f4d5e7e0ecc0e97a43402f181c85d75d7c12
GusevTimofey <gusev.t.s1@gmail.com>
2021-08-28 19:35:51 +0300

    docker compose

--- tracker/src/Tracker/Services/SettingsReader.hs
+++ tracker/src/Tracker/Services/SettingsReader.hs
@@ -17,5 +17,5 @@ read' :: IO AppSettings
 read' = do
     let httpSettings = HttpSettings "0.0.0.0" 8089 
         reqPeriodSettings = BlockRequestSettings 0
-        kafkaSettings = KafkaProducerSettings "amm-topic" "proxy-topic" ["0.0.0.0:9092"] "default-proxy-key" "default-amm-key" 
+        kafkaSettings = KafkaProducerSettings "amm-topic" "proxy-topic" ["kafka:9092"] "default-proxy-key" "default-amm-key" 
     pure $ AppSettings httpSettings reqPeriodSettings kafkaSettings
