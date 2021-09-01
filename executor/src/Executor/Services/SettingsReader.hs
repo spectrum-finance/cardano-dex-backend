f39d79d0692e3b43022b3d1e4a95df144602991c
GusevTimofey <gusev.t.s1@gmail.com>
2021-09-01 19:09:15 +0300

    cleaned up

--- executor/src/Executor/Services/SettingsReader.hs
+++ executor/src/Executor/Services/SettingsReader.hs
@@ -13,11 +13,3 @@ data SettingsReader = SettingsReader
 
 mkSettingsReader :: SettingsReader
 mkSettingsReader = SettingsReader $ input auto "./executor/resources/config.dhall"
-
--- read' :: IO AppSettings
--- read' = do
---     cfg <- input auto "./configs/config.dhall"
---     pure 
---     let kafkaS = KafkaConsumerSettings ["kafka:9092"] "executor_group_id_1" ["proxy-topic"] 1000 1
---         httpS  = HttpSettings "resolver" 8088
---     pure $ AppSettings kafkaS httpS
