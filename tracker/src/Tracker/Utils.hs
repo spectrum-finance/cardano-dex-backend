e293d071b52c3af6a6d9d330aea94d21b6523cbc
GusevTimofey <gusev.t.s1@gmail.com>
2021-10-10 19:57:03 +0300

    toFullTxOut

--- tracker/src/Tracker/Utils.hs
+++ tracker/src/Tracker/Utils.hs
@@ -6,10 +6,22 @@ module Tracker.Utils
 import RIO
 import Tracker.Models.ExplorerModels
 import Dex.Models
+import Plutus.V1.Ledger.Address 
+import Plutus.V1.Ledger.Credential
+import Plutus.V1.Ledger.Scripts  (ValidatorHash(..))
+import Data.ByteString.Char8 as Data
 
 unsafeFromEither :: Either String a -> a
 unsafeFromEither (Left err)    = error err
 unsafeFromEither (Right value) = value
 
 toFullTxOut :: ApiFullTxOut -> FullTxOut
-toFullTxOut _ = undefined
+toFullTxOut ApiFullTxOut{..} = 
+  FullTxOut {
+    outGId           = outGId,
+    refId            = refId,
+    refIdx           = refIdx, -- ^ Index into the referenced transaction's outputs
+    txOutAddress     = scriptHashAddress (ValidatorHash $ Data.pack txOutAddress),
+    txOutValue       = jsValue,
+    fullTxOutDatum   = datum
+  }
