2fb7c4665a8265d824c92192a145bc4edeaeedec
Bromel777 <33941049+Bromel777@users.noreply.github.com>
2023-08-09 19:57:22 +0300

    add missing files (#72)

---
+++ amm-executor/src/Spectrum/Executor/OrdersExecutor/RefInputs.hs
@@ -0,0 +1,34 @@
+module Spectrum.Executor.OrdersExecutor.RefInputs where
+
+import CardanoTx.Models (FullTxOut)
+import Spectrum.Executor.Config (TxRefs(..))
+import RIO
+import Explorer.Service (Explorer(..))
+import qualified Ledger.Tx.CardanoAPI as Interop
+import Explorer.Class (ToCardanoTx(toCardanoTx))
+
+data RefInputs = RefInputs
+  { swapOutput    :: FullTxOut
+  , redeemOutput  :: FullTxOut
+  , depositOutput :: FullTxOut
+  , poolOutput    :: FullTxOut
+  }
+
+data CouldnotRetriveRefInputs = CouldnotRetriveRefInputs deriving Show
+instance Exception CouldnotRetriveRefInputs
+
+mkRefInputs :: (MonadIO m, MonadThrow m) => TxRefs -> Explorer m -> m RefInputs
+mkRefInputs TxRefs{..} Explorer{..} = do
+  let
+    poolOutRef = Interop.fromCardanoTxIn poolRef
+    depositOutRef = Interop.fromCardanoTxIn depositRef
+    redeemOutRef = Interop.fromCardanoTxIn redeemRef
+    swapOutRef = Interop.fromCardanoTxIn swapRef
+  poolRefOuput  <- getOutput poolOutRef
+  depositRefOut <- getOutput depositOutRef
+  redeemRefOut  <- getOutput redeemOutRef
+  swapRefOut    <- getOutput swapOutRef
+  case catMaybes [swapRefOut, redeemRefOut, depositRefOut, poolRefOuput] <&> toCardanoTx of
+      [swap, redeem, deposit, pool] ->
+          pure $ RefInputs swap redeem deposit pool
+      _ -> throwM CouldnotRetriveRefInputs
