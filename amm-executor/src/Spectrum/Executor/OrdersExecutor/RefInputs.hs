2a20537393eb8113255fc6bdcfe80a5cfe621c76
Bromel777 <alexbromel@gmail.com>
2023-09-11 10:51:22 +0200

    fix ref inputs

--- amm-executor/src/Spectrum/Executor/OrdersExecutor/RefInputs.hs
+++ amm-executor/src/Spectrum/Executor/OrdersExecutor/RefInputs.hs
@@ -11,7 +11,8 @@ data RefInputs = RefInputs
   { swapOutput    :: FullTxOut
   , redeemOutput  :: FullTxOut
   , depositOutput :: FullTxOut
-  , poolOutput    :: FullTxOut
+  , poolOutputV1  :: FullTxOut
+  , poolOutputV2  :: FullTxOut
   }
 
 data CouldnotRetriveRefInputs = CouldnotRetriveRefInputs deriving Show
@@ -20,15 +21,17 @@ instance Exception CouldnotRetriveRefInputs
 mkRefInputs :: (MonadIO m, MonadThrow m) => TxRefs -> Explorer m -> m RefInputs
 mkRefInputs TxRefs{..} Explorer{..} = do
   let
-    poolOutRef = Interop.fromCardanoTxIn poolV1Ref
+    poolOutV1Ref = Interop.fromCardanoTxIn poolV1Ref
+    poolOutV2Ref = Interop.fromCardanoTxIn poolV2Ref
     depositOutRef = Interop.fromCardanoTxIn depositRef
     redeemOutRef = Interop.fromCardanoTxIn redeemRef
     swapOutRef = Interop.fromCardanoTxIn swapRef
-  poolV1RefOuput  <- getOutput poolOutRef
+  poolV1RefOuput  <- getOutput poolOutV1Ref
+  poolV2RefOutput <- getOutput poolOutV2Ref
   depositRefOut <- getOutput depositOutRef
   redeemRefOut  <- getOutput redeemOutRef
   swapRefOut    <- getOutput swapOutRef
-  case catMaybes [swapRefOut, redeemRefOut, depositRefOut, poolV1RefOuput] <&> toCardanoTx of
-      [swap, redeem, deposit, pool] ->
-          pure $ RefInputs swap redeem deposit pool
+  case catMaybes [swapRefOut, redeemRefOut, depositRefOut, poolV1RefOuput, poolV2RefOutput] <&> toCardanoTx of
+      [swap, redeem, deposit, poolV1, poolV2] ->
+          pure $ RefInputs swap redeem deposit poolV1 poolV2
       _ -> throwM CouldnotRetriveRefInputs
